8b29670bda2bad52b992377c8730d446b16dad62
Karthik <karthik180499@gmail.com>
2020-08-14 12:15:29 +0530

    assessment5-final

--- assessment5/src/app/component/user/user.component.ts
+++ assessment5/src/app/component/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient,private router:Router) { }
+  email;
+  logintime;
+  url = "http://127.0.0.1:6500/usertime";
+  final;
   ngOnInit(): void {
+
+    this.http.get(this.url).subscribe((resp) => {
+     console.log(resp);
+     this.final=JSON.stringify(resp);
   }
 
 }
